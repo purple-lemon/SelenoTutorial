f5191b922b5024d606080c7e667409198b94565b
byurch <byurchak@gmail.com>
2018-05-01 14:58:15 +0300

    Test steps

---
+++ PL_Automation/Auto.UnitTests/UserLoginTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Auto.UnitTests
+{
+    public class UserLoginTests
+    {
+		[Fact]
+		public void SuccessLogin()
+		{
+
+		}
+
+		[Fact]
+		public void IncorrectLogin()
+		{
+
+		}
+    }
+}
