adad14e47b4b3c59c0ab8fa4f1ce562eb3786453
byurch <byurchak@gmail.com>
2018-07-06 14:46:58 +0300

    Screenshot emulator

---
+++ XunitTestReports/Verify.Tests/BaseTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Verify.Tests
+{
+    public class BaseTest
+    {
+        private readonly ITestOutputHelper output;
+        public BaseTest(ITestOutputHelper testOutputHelper)
+        {
+            output = testOutputHelper;
+        }
+        protected void UITest(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                //var screenshot = Driver.TakeScreenshot();
+
+                var filePath = @"D:\Screenshots\test.txt";
+
+                File.WriteAllText(filePath, "Here is my screenshot");
+                output.WriteLine("Screenshot stored: " + filePath);
+                // This would be a good place to log the exception message and
+                // save together with the screenshot
+
+                throw;
+            }
+        }
+    }
+}
--- XunitTestReports/Verify.Tests/CalcTests.cs
+++ XunitTestReports/Verify.Tests/CalcTests.cs
@@ -5,26 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 using Verify.Code;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Verify.Tests
 {
     [Collection("Calculator tests")]
-    public class CalcTests
+    public class CalcTests : BaseTest
     {
+
+        private readonly ITestOutputHelper output;
+        public CalcTests(ITestOutputHelper testOutputHelper)
+        {
+            output = testOutputHelper;
+        }
         [Fact(DisplayName = "Test sum")]
         public void SumTest()
         {
-            var ticks = DateTime.Now.Ticks;
-            var date = DateTime.Now;
-            var secs = date.Second + 60 * date.Minute + 3600 * date.Hour;
-            var calc = new Calculator();
-            Assert.Equal(5, calc.Sum(2, 3));
+            UITest(() =>
+            {
+                var ticks = DateTime.Now.Ticks;
+                var date = DateTime.Now;
+                var secs = date.Second + 60 * date.Minute + 3600 * date.Hour;
+                var calc = new Calculator();
+                try
+                {
+                    Assert.Equal(5, calc.Sum(2, 4));
+                }
+                catch (Exception e)
+                {
+                    output.WriteLine("Link to screenshot: http://test.com/123");
+                    throw;
+                }
+            });
         }
 
         [Fact(DisplayName = "Test substraction")]
         public void SubTest()
         {
             var calc = new Calculator();
+
             Assert.Equal(5, calc.Substract(8, 3));
         }
 
